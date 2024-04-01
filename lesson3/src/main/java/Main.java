daa08771befd42e3aeb427e16dd1467bd51562fd
sergey.makarov <sergey.makarov@avotech.uz>
2024-04-01 21:18:19 +0300

    lesson 5 add l3,4 small word

--- lesson3/src/main/java/Main.java
+++ lesson3/src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Lesson 3");
+        System.out.println("Hello lesson 3");
+        System.out.println("Hello lesson 4");
     }
 }
