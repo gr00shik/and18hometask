36bc88feeb629dd7875506419b89678f73ed048b
sergey.makarov <sergey.makarov@avotech.uz>
2024-04-01 21:16:57 +0300

    lesson 4 add l1,2

--- lesson3/src/main/java/Main.java
+++ lesson3/src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello Lesson 1");
+        System.out.println("Hello Lesson 2");
         System.out.println("Hello Lesson 3");
     }
 }
